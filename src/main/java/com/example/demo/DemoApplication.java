5e356b549c37be4a9e5ed489b9d6733594770e6d
Fabiano Carneiro <136657746+FABIANO-DA-SILVA-CARNEIRO@users.noreply.github.com>
2024-03-18 14:28:35 -0300

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -18,27 +18,19 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
-
 		String userInput = args[0]; // Supondo que userInput seja a entrada do usuário
 
-	        // Conexão com o banco de dados (apenas para fins de exemplo)
-	        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/banco", "usuario", "senha")) {
-	            Statement statement = connection.createStatement();
+	    	Statement statement = connection.createStatement();
+	        
+	    	String query = "SELECT * FROM usuarios WHERE nome = '" + userInput + "'";
 	            
-	            // Consulta SQL vulnerável à injeção
-	            String query = "SELECT * FROM usuarios WHERE nome = '" + userInput + "'";
-	            
-	            // Execução da consulta
-	            ResultSet resultSet = statement.executeQuery(query);
-	
-	            // Processamento dos resultados
-	            while (resultSet.next()) {
-	                String nome = resultSet.getString("nome");
-	                String email = resultSet.getString("email");
-	                System.out.println("Nome: " + nome + ", Email: " + email);
-	            }
-		
+	    	ResultSet resultSet = statement.executeQuery(query);
 
+	   	 while (resultSet.next()) {
+			String nome = resultSet.getString("nome");
+			String email = resultSet.getString("email");
+			System.out.println("Nome: " + nome + ", Email: " + email);
+	   	 }
 	}
        
 	@GetMapping("/")
