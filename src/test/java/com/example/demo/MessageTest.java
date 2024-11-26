f5bcff8395c7244b824044680a2df47d764a7bc3
Fabiano Carneiro <136657746+FABIANO-DA-SILVA-CARNEIRO@users.noreply.github.com>
2024-11-26 11:27:55 -0300

    Update MessageTest.java

--- src/test/java/com/example/demo/MessageTest.java
+++ src/test/java/com/example/demo/MessageTest.java
@@ -1,10 +1,18 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest   
 public class MessageTest {
+   
    @Test
    public void messageOk1() {
        Message message = new Message();
        String result = message.message(5);
        assertEquals("YES", result);
    }
+   
    @Test
    public void messageOk2() {
        Message message = new Message();
