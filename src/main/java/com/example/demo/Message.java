e64211090af041d0aca8142ffebf615d404bd3b5
Fabiano Carneiro <136657746+FABIANO-DA-SILVA-CARNEIRO@users.noreply.github.com>
2024-11-26 10:50:38 -0300

    Create Message.java

---
+++ src/main/java/com/example/demo/Message.java
@@ -0,0 +1,19 @@
+package org.example;
+
+
+public class Message {
+
+    private static final int MIN_VALUE = 0;
+    private static final int MAX_VALUE = 10;
+
+    public String getMessage(int number) {
+        if (isInRange(number)) {
+            return "YES";
+        } else {
+            return "NO";
+        }
+    }
+
+    private boolean isInRange(int number) {
+        return number >= MIN_VALUE && number <= MAX_VALUE;
+    }
