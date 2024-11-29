db6c99613b8313b8ecc100ec18e8b981acce0a76
Fabiano Carneiro <136657746+FABIANO-DA-SILVA-CARNEIRO@users.noreply.github.com>
2024-11-29 10:40:30 -0300

    Update MessageTest.java

--- src/test/java/com/example/demo/MessageTest.java
+++ src/test/java/com/example/demo/MessageTest.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.example.demo.Message; 
+
 @SpringBootTest   
 public class MessageTest {
    
