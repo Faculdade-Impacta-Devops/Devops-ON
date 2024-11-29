2db34f98c1e003667b098fda4612622c207ebf7a
Fabiano da Silva Carneiro <fabiano.carneiro@f1rst.com.br>
2024-11-29 11:00:26 -0300

    Refactor MessageTest to use getMessage method

--- src/test/java/com/example/demo/MessageTest.java
+++ src/test/java/com/example/demo/MessageTest.java
@@ -1,7 +1,8 @@
-
 package com.example.demo;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.example.demo.Message; 
 
@@ -11,14 +12,14 @@ public class MessageTest {
    @Test
    public void messageOk1() {
        Message message = new Message();
-       String result = message.message(5);
+       String result = message.getMessage(5);
        assertEquals("YES", result);
    }
    
    @Test
    public void messageOk2() {
        Message message = new Message();
-       String result = message.message(-5);
+       String result = message.getMessage(-5);
        assertEquals("NO", result);
    }
 }
